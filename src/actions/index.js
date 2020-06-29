6c4ca69a7ddbf16d7f39d5c57a95beedb10aa952
Jovan Novakovic <zmaj.joca@gmail.com>
2020-06-29 12:16:17 +0200

    Fetching singular records

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,11 @@ export const fetchPosts = () => {
 	return async (dispatch) => {
 		const response = await jsonPlaceholder.get('/posts');
 
-		dispatch({ type: 'FETCH_POSTS', payload: response });
+		dispatch({ type: 'FETCH_POSTS', payload: response.data });
 	};
+};
+//isto ko gore samo sa es2015(ES6) skraceno za return i bez {}
+export const fetchUser = (id) => async (dispatch) => {
+	const response = await jsonPlaceholder.get(`/users/${id}`);
+	dispatch({ type: 'FETCH_USER', payload: response.data });
 };
