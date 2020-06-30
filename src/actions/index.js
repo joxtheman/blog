c18cff984c8af5d6372d0f8540b954808799d525
Jovan Novakovic <zmaj.joca@gmail.com>
2020-06-30 11:25:25 +0200

    list of users

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,7 +7,7 @@ export const fetchPosts = () => {
 		dispatch({ type: 'FETCH_POSTS', payload: response.data });
 	};
 };
-//isto ko gore samo sa es2015(ES6) skraceno za return i bez {} mada
+//isto ko gore samo sa es2015(ES6) skraceno za return i bez{}
 export const fetchUser = (id) => async (dispatch) => {
 	const response = await jsonPlaceholder.get(`/users/${id}`);
 	dispatch({ type: 'FETCH_USER', payload: response.data });
